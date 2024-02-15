5f5e4d270079413f35a2d93dd0b5268792c60713
Derek Ng <37806620+dng24@users.noreply.github.com>
2024-02-15 14:43:11 -0500

    receive response code from server side channel

--- internal/network/sidechannel.go
+++ internal/network/sidechannel.go
@@ -10,7 +10,21 @@ import (
 
 const (
     sideChannelPort = 55556
-    ask4PermissionOpcode = 2
+)
+
+type opcode byte // request type to the server
+
+const (
+    ask4permission opcode = iota
+    mobileStats
+)
+
+type responseCode byte // code representing the status of a response back from the server
+
+const (
+    okResponse responseCode = iota
+    clientInputError
+    serverError
 )
 
 type SideChannel struct {
@@ -64,7 +78,7 @@ func (sideChannel SideChannel) SendID(userID string, replayID int, replayName st
 //     samples per replay is returned as the info; if status is failure, then failure code is
 //     returned as the info; can also return errors
 func (sideChannel SideChannel) Ask4Permission() ([]string, error) {
-    resp, err := sideChannel.sendAndReceive(ask4PermissionOpcode, "")
+    resp, err := sideChannel.sendAndReceive(ask4permission, "")
     if err != nil {
         return nil, err
     }
@@ -84,8 +98,8 @@ func (sideChannel SideChannel) CleanUp() {
 // opcode: the operation number
 // message: the data to send to the server
 // Returns the server response or any errors
-func (sideChannel SideChannel) sendAndReceive(opcode int, message string) (string, error) {
-    buffer := []byte{byte(opcode)}
+func (sideChannel SideChannel) sendAndReceive(op opcode, message string) (string, error) {
+    buffer := []byte{byte(op)}
     buffer = append(buffer, []byte(message)...)
     fmt.Println("sending:", buffer)
     _, err := sideChannel.conn.Write(buffer)
@@ -97,6 +111,19 @@ func (sideChannel SideChannel) sendAndReceive(opcode int, message string) (strin
     n, err := sideChannel.conn.Read(resp)
     if err != nil {
         return "", err
+    }
+
+    responseCode := resp[0]
+    fmt.Println("response code:", responseCode)
+    if responseCode == byte(okResponse) {
+        fmt.Println("receiving:", string(resp[1:n]))
+        return string(resp[1:n]), nil
+    } else if responseCode == byte(clientInputError) {
+        return "", fmt.Errorf("Bad input received from client.")
+    } else if responseCode == byte(serverError) {
+        return "", fmt.Errorf("Server unable to process request.")
+    } else {
+        return "", fmt.Errorf("Unknown error.")
     }
     fmt.Println("receiving:", string(resp[:n]))
     return string(resp[:n]), nil
